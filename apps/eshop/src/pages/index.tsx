e83d725f0a600a111e778559955651cebacacf90
rakibkhanofficial <khanrakib218@gmail.com>
2023-08-15 23:31:34 +0600

    fix: adding some changes

--- apps/eshop/src/pages/index.tsx
+++ apps/eshop/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
-import react from 'react'
+import react from "react";
 
 export default function Home() {
   return (
     <main className="bg-black">
-      <h1 className="text-3xl font-bold text-black text-center justify-center flex">Hello World</h1>
+      <h1 className="text-3xl font-bold text-white text-center justify-center flex">
+        Hello World
+      </h1>
+      <h1 className="text-3xl font-bold text-white text-center justify-center flex">
+        Hello Eshop
+      </h1>
     </main>
   );
 }
