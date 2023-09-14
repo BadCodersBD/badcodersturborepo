19114b3ce7f934b74bd2fa109fa6894748dd32bb
rakibkhanofficial <khanrakib218@gmail.com>
2023-09-14 10:42:09 +0600

    refactor: update endpoints

--- apps/eshop/src/utils/api/route.ts
+++ apps/eshop/src/utils/api/route.ts
@@ -1,30 +1,4 @@
 export const endPoints = {
-  akibfb: {
-    emailToCustomer: '/customers/find-by-email',
-    orderHistoryOfCustomer: '/orders/order-history',
-    wishlistCreate: '/wishlist/create',
-    wishlistDetails: '/wishlist/find-by-customer',
-    wishlistDelete: '/wishlist/delete',
-  },
-  akijecom: {
-    orderCreate: '/orders/create',
-    shippingInfo: '/shippingInfo/create',
-    sslPayment: '/ssl-payment',
-    orderDetails: '/order-details/find-by-order-id',
-    addressFindbyCustomer: '/address/find-by-customer',
-    addressUpdate: '/address/update',
-    cashOnDelivery: '/payment/cash-on-delivery',
-    hubs: '/hub',
-    createHub: '/hub/create',
-    assignHub: '/admin-orders/assign-hub',
-    deliveryman: '/delivery-man',
-    assignDeliveryman: '/admin-orders/assign-delivery-man',
-    deliverymanOrderLists: '/delivery-man/delivery-Man',
-    deliverymanOrderComplete: '/delivery-man/delivered-order',
-    createDeliveryMan: 'delivery-man-auth/register',
-    stockIn: '/transaction/create',
-    allStockProductWithHub: '/transaction/running-balance-by-product',
-  },
   auth: {
     register: '/auth/register',
     login: '/auth/login',
@@ -43,8 +17,5 @@ export const endPoints = {
       login: '/delivery-man-auth/login',
     },
     resendOTP: '/delivery-man-otp',
-  },
-  visitor: {
-    userDeviceInfo: '/client-info/create',
   },
 }
