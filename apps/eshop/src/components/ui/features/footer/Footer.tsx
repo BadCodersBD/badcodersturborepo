5f9c6126337e40d6cbcc66c7def9f0bcba0b778f
rakibkhanofficial <khanrakib218@gmail.com>
2023-09-18 09:11:43 +0600

    feat: added footer section

--- apps/eshop/src/components/ui/features/footer/Footer.tsx
+++ apps/eshop/src/components/ui/features/footer/Footer.tsx
@@ -1,16 +1,40 @@
-import React from "react";
-import Image from "next/image";
+import { GridColumn } from "../../element/GridColumn/GridColumn";
 import { Styled } from "./Footer.styled";
 import Grid from "@mui/material/Grid";
-import { GridColumn } from "../../element/GridColumn/GridColumn";
+import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+import SocialIcon from "./SocialIcon/SocialIcon";
 
 const Footer = () => {
   return (
     <>
       <Styled.Footer>
-        <div>Design & Develop By @Rakib Khan ©2023</div>
-        <div>For any Development Contact Me</div>
+        <Grid container rowSpacing={2}>
+          <Grid item lg={4}>
+            <div>Design & Develop By @Rakib Khan ©2023</div>
+            <div>For any Development Contact Me</div>
+          </Grid>
+        </Grid>
+        <Grid container rowSpacing={2}>
+          <Grid item lg={4}>
+            <div>Design & Develop By @Rakib Khan ©2023</div>
+            <div>For any Development Contact Me</div>
+          </Grid>
+        </Grid>
+        <Grid container rowSpacing={2}>
+          <Grid item lg={4}>
+            <div>Design & Develop By @Rakib Khan ©2023</div>
+            <div>For any Development Contact Me</div>
+          </Grid>
+        </Grid>
+        <Grid container rowSpacing={2}>
+          <Grid item lg={4}>
+            <div>Design & Develop By @Rakib Khan ©2023</div>
+            <div>For any Development Contact Me</div>
+            <SocialIcon/>
+          </Grid>
+        </Grid>
       </Styled.Footer>
     </>
   );
