720625b22726a6f9e2f0f56ca3fa8fdf4c9ee0e7
rakibkhanofficial <khanrakib218@gmail.com>
2023-09-22 10:24:55 +0600

    refactor: trying to fix build error

--- apps/server/src/controllers/userController.ts
+++ apps/server/src/controllers/userController.ts
@@ -1,19 +1,18 @@
-// controllers/userController.ts
-
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
+// import { requiresAuth } from '../middleware/auth'; 
 
 export const getAuthenticatedUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const user = await User.findById(req.user.userId).select('name email image').exec();
-  
-      if (!user) {
-        throw new Error('User not found');
-      }
-  
-      res.json({ user });
+        // Assuming req.user is properly attached by requiresAuth middleware
+        const user = await User.findById(req.user?.userId).select('name email image').exec();
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        res.json({ user });
     } catch (error) {
-      next(error); // Pass the error to the error handling middleware
+        next(error); // Pass the error to the error handling middleware
     }
-  };
-  
+};
