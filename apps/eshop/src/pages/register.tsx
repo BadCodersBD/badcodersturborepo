8dc052f16460557108d009dd0707d456b949e66e
rakibkhanofficial <khanrakib218@gmail.com>
2023-10-07 20:12:23 +0600

    feat: added signup setup

--- apps/eshop/src/pages/register.tsx
+++ apps/eshop/src/pages/register.tsx
@@ -1,35 +1,71 @@
-import TermsConditions from "../components/elements/InputElements/TermsConditions";
-import { useRegister } from "@/utils/Registration/useRegister";
-import { SocialSignIn } from "@/components/elements/InputElements/SocialSignIn";
-// import { AssociateCompanyLogo } from "@/components/elements/AssociatedCompanyLogo/AssociatedCompanyLogo";
-import { Email } from "@/components/elements/InputElements/Email";
-import { Password } from "@/components/elements/InputElements/Password";
-import { ConfirmPassword } from "@/components/elements/InputElements/ConfirmPassword";
-// import { Captcha } from "@/components/elements/InputElements/Captcha";
 import { LoginLink } from "@/components/elements/InputElements/LoginLink";
-import { useInputSave } from "@/utils/Registration/useInputSave";
 import { FormWrapper } from "@/components/elements/InputElements/FormWrapper";
 import Head from "next/head";
 import Header from "@/components/ui/features/Header/Header";
 import Footer from "@/components/ui/features/footer/Footer";
 import Link from "next/link";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Input} from 'antd';
 import metaData from "../../public/meta.json";
+import { Input } from "antd";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 const RegisterForm = () => {
-  const {
-    register,
-    handleSubmit,
-    errors,
-    submitting,
-    registerError,
-    // captchaRef,
-    registerUserData,
-    onSubmit,
-  } = useRegister();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false); // Added loading state
+  const [nameInput, setnameInput] = useState("");
+  const [emailInput, setemailInput] = useState("");
+  const [passwordInput, setpasswordInput] = useState("");
 
-  const { localData, handleChange } = useInputSave();
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleData = (e: any) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+    if (name === "username") setnameInput(value);
+    if (name === "email") setemailInput(value);
+    if (name === "password") setpasswordInput(value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const jsonData = JSON.stringify({
+      username: data?.username,
+      email: data?.email,
+      password: data?.password,
+    });
+
+    try {
+      const response = await fetch(
+        "https://carrentalserver.vercel.app/api/users/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: jsonData,
+        }
+      );
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData); // Add this line
+        router.push("/");
+      } else {
+        console.error("Unexpected response:", response);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="bg-white">
@@ -41,112 +77,74 @@ const RegisterForm = () => {
         <meta property="og:url" content={metaData.url} />
         <meta property="og:type" content={metaData.type} />
         <meta property="og:image" content={metaData.image} />
-        <meta name="google-site-verification" content="uF7IpXKCuelQJy81bb-K1zcDj1VXO3l366zJzWjRki8" /> 
+        <meta
+          name="google-site-verification"
+          content="uF7IpXKCuelQJy81bb-K1zcDj1VXO3l366zJzWjRki8"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <div className="py-20 px-10">
-      <Link href="/">
-        <div className=" border text-black border-green-700 w-40 h-auto mt-5 p-3 hover:bg-lime-600 hover:text-white hover:transition rounded-md">
-          <ArrowBackIcon />
-          Back to Home
-        </div>
-      </Link>
-    <FormWrapper>
-      <form
-        className="flex flex-col space-y-1 px-1 md:space-y-1 lg:space-y-1"
-        action="#"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="pb-1">
-          <label htmlFor="name" className="registerLabel">
-            Name
-          </label>
-          <Input
-            type="text"
-            id="name"
-            name="name"
-            className="registerTextInput"
-            style={{
-              outline: errors.name ? "0.125rem solid rgb(220, 38, 38)" : "",
-            }}
-            placeholder="name"
-            defaultValue={localData?.name}
-            {...register("name", { required: "Name is required" })}
-            onChange={(e) => handleChange(e)}
-          />
-          {errors.name && (
-            <span className="text-xs text-red-600">{errors.name.message}</span>
-          )}
-        </div>
-        <Email register={register} errors={errors} />
-        <Password
-          register={register}
-          errors={errors}
-          label="Password"
-          placeholder="enter your password"
-        />
-        <ConfirmPassword
-          register={register}
-          errors={errors}
-          label="Confirm password"
-          placeholder="confirm your password"
-        />
-        <div className="flex items-start">
-          <div className="flex h-5 items-center">
+        <Link href="/">
+          <div className=" border text-black border-green-700 w-40 h-auto mt-5 p-3 hover:bg-lime-600 hover:text-white hover:transition rounded-md">
+            <ArrowBackIcon />
+            Back to Home
+          </div>
+        </Link>
+        <FormWrapper>
+          <form
+            className="flex flex-col space-y-1 px-1 md:space-y-1 lg:space-y-1"
+            action="#"
+            onSubmit={handleSubmit}
+          >
+            <div className="pb-1">
+              <label htmlFor="name" className="registerLabel">
+                Name
+              </label>
+              <Input
+                type="text"
+                placeholder="Enter Your Name"
+                id="name"
+                name="username"
+                value={nameInput}
+                onChange={handleData}
+              />
+            </div>
             <Input
-              id="policy"
-              type="checkbox"
-              style={{
-                outline: errors.policy ? "0.125rem solid rgb(220, 38, 38)" : "",
-              }}
-              {...register("policy", { required: true })}
-              className="registerCheckbox"
+              type="email"
+              title="email"
+              name="email"
+              placeholder="Enter Your Email"
+              value={emailInput}
+              onChange={handleData}
             />
+            <Input
+              type="password"
+              title="password"
+              name="password"
+              value={passwordInput}
+              required
+              placeholder="enter your password"
+              onChange={handleData}
+            />
+            <div className="px-8 bg-cyan-700 hover:bg-cyan-500 py-3 rounded-md text-center">
+              <button
+                type="submit"
+                className="registerButton"
+                disabled={isLoading}
+                onClick={handleSubmit}
+              >
+                {isLoading ? "Registering..." : "Register"}
+              </button>
+            </div>
+          </form>
+          <div className="px-1">
+            {/* <SocialSignIn /> */}
+            <LoginLink title="Already have an account?" />
           </div>
-          <TermsConditions />
-        </div>
-        {errors.policy && (
-          <span className="text-xs text-white">
-            Please accept the terms and conditions
-          </span>
-        )}
-        <div className="flex items-center justify-center">
-          {/* <Captcha captchaRef={captchaRef} /> */}
-        </div>
-        {registerError && (
-          <span className="text-xs text-red-600">{registerError}</span>
-        )}
-        {registerUserData && registerUserData !== 200 && (
-          <span className="text-xs text-red-600">
-            {registerUserData === 401 && (
-              <LoginLink title="Email or Phone already exists!" />
-            )}
-          </span>
-        )}
-        {registerUserData === 200 ? (
-          <h3 className="text-center text-2xl text-blue-400">
-            Customer registered successfully!
-          </h3>
-        ) : (
-          <div className="px-8 bg-cyan-700 hover:bg-cyan-500 py-3 rounded-md text-center">
-            <button
-              type="submit"
-              className="registerButton"
-              disabled={submitting}
-            >
-              {submitting ? "Registering..." : "Register"}
-            </button>
-          </div>
-        )}
-      </form>
-      <div className="px-1">
-        <SocialSignIn />
-        <LoginLink title="Already have an account?" />
+        </FormWrapper>
       </div>
-    </FormWrapper>
-    </div>
-    <Footer />
+      <Footer />
     </div>
   );
 };
