bdd8fcc890058daf4364bfa4a152ffd1b5766cf9
rakibkhanofficial <khanrakib218@gmail.com>
2023-10-16 16:46:02 +0600

    fix: fix some error

--- apps/eshop/src/pages/allcars.tsx
+++ apps/eshop/src/pages/allcars.tsx
@@ -17,10 +17,11 @@ const AllCars = () => {
         <meta property="og:url" content={metaData.url} />
         <meta property="og:type" content={metaData.type} />
         <meta property="og:image" content={metaData.image} />
-        <meta name="google-site-verification" content="K81iN0RCpi5NOoiQmvPbDGfFInM19PeDj0yrYEM3P-8" />        <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="K81iN0RCpi5NOoiQmvPbDGfFInM19PeDj0yrYEM3P-8" />   
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Allcars/>
+      <Allcars product={[]}/>
       <Footer />
     </>
   );
