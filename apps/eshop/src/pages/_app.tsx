5a6435a70164735fe0fa427b808196bdfd4b2732
rakibkhanofficial <khanrakib218@gmail.com>
2023-10-16 17:37:11 +0600

    fix: add tag manager google

--- apps/eshop/src/pages/_app.tsx
+++ apps/eshop/src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      // Trigger GTM pageview
+      window.dataLayer?.push({ event: 'pageview', url });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Clean up event listener
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }
