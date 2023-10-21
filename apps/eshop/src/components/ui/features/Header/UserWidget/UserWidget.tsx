ce337e84e2e4b393a7eeedd5b52d110860049dbf
rakibkhanofficial <khanrakib218@gmail.com>
2023-10-21 06:22:50 +0600

    feat: add new feature car rental api intregation

--- apps/eshop/src/components/ui/features/Header/UserWidget/UserWidget.tsx
+++ apps/eshop/src/components/ui/features/Header/UserWidget/UserWidget.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Styled } from "../Header.styled";
-import Link from "next/link";
+import { Styled } from "./UserWidget.styled";
 import { useRouter } from "next/router";
 import Cookies from "universal-cookie";
 
@@ -8,10 +7,10 @@ const UserWidget = () => {
   const router = useRouter();
   const cookies = new Cookies();
 
-  console.log("cookies user data", cookies.get("userData"));
+  // console.log("cookies user data", cookies.get("userData"));
 
   return (
-    <div className="flex space-x-4 py-5 font-semibold">
+    <div className="flex gap-4 py-5 font-semibold">
       {cookies.get("userData") ? (
         <>
           <Styled.LoginButton
@@ -20,7 +19,6 @@ const UserWidget = () => {
               // cookies.set("userData", null);
               router.push("/profile");
             }}
-            className="rounded-3xl border-2 w-full h-full py-0.5 px-3"
           >
             My Profile
           </Styled.LoginButton>
@@ -30,29 +28,25 @@ const UserWidget = () => {
               cookies.set("userData", null);
               router.push("/login");
             }}
-            className="rounded-3xl border-2 w-full h-full py-0.5 px-3"
           >
             Logout
           </Styled.LoginButton>
         </>
       ) : (
         <>
-          {" "}
           <Styled.LoginButton
             onClick={(e) => {
               e.preventDefault();
               router.push("/login");
             }}
-            className="rounded-3xl border-2 w-full h-full py-0.5 px-3"
           >
-            Log In
+            LogIn
           </Styled.LoginButton>
           <Styled.RegistrationButton
             onClick={(e) => {
               e.preventDefault();
               router.push("/register");
             }}
-            className="rounded-3xl border-2 w-full h-full py-0.5 px-3"
           >
             Sign Up
           </Styled.RegistrationButton>
--- apps/server/src/controllers/CarrentalController.ts
+++ apps/server/src/controllers/CarrentalController.ts
@@ -33,15 +33,17 @@ interface CreateRentalBody {
     userId: string,
     carModel?: string,
     pickuplocation?: string,
+    pickuptime?: string,
     dropofflocation?: string,
-    mobilenumber?: string,
+    mobilenumber?: number,
     startDate?: Date,
     endDate?: Date,
+    rentalprice?: number
 }
 
 export const createRental: RequestHandler<unknown, unknown, CreateRentalBody, unknown> = async (req, res) => {
     try {
-        const { userId, carModel, pickuplocation, dropofflocation, mobilenumber, startDate, endDate } = req.body;
+        const { userId, carModel, pickuplocation, pickuptime, dropofflocation, mobilenumber, startDate, endDate, rentalprice } = req.body;
     
         // Check if the user with the provided userId exists
         const user = await UserModel.findById(userId);
@@ -54,10 +56,12 @@ export const createRental: RequestHandler<unknown, unknown, CreateRentalBody, un
           userId,
           carModel,
           pickuplocation,
+          pickuptime,
           dropofflocation,
           mobilenumber,
           startDate,
           endDate,
+          rentalprice
         });
     
         res.status(201).json({ message: "Car rental record created", carRental: newCarRental });
@@ -71,6 +75,7 @@ export const createRental: RequestHandler<unknown, unknown, CreateRentalBody, un
 export const updateRental: RequestHandler = async (req, res, next) => {
     const rentalId = req.params.rentalId;
     const newpickuplocation = req.body.pickuplocation;
+    const newpickuptime = req.body.pickuptime;
     const newdropofflocation = req.body.dropofflocation;
     const newmobilenumber = req.body.mobilenumber;
     const newCarModel = req.body.carModel;
@@ -89,6 +94,7 @@ export const updateRental: RequestHandler = async (req, res, next) => {
         }
 
         rental.carModel = newCarModel;
+        rental.pickuptime = newpickuptime;
         rental.pickuplocation = newpickuplocation;
         rental.dropofflocation = newdropofflocation;
         rental.mobilenumber = newmobilenumber;
