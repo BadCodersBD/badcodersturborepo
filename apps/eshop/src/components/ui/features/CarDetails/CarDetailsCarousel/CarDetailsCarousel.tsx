236278ecf291fe71f5fd589420009b7703cd7088
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2024-01-03 00:08:48 +0600

    feat: styled componet added and change design

--- apps/eshop/src/components/ui/features/CarDetails/CarDetailsCarousel/CarDetailsCarousel.tsx
+++ apps/eshop/src/components/ui/features/CarDetails/CarDetailsCarousel/CarDetailsCarousel.tsx
@@ -12,44 +12,46 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Image from "next/image";
+import { Styled } from "./CarDetailsCarousel.styled";
 
 const CarDetailsCarousel = () => {
 	return (
-		<div>
-			{/* <Image
-        src={}
-         /> */}
+		<Styled.Main>
 			<Swiper
 				autoplay={{
-					delay: 2500,
+					delay: 4500,
 					disableOnInteraction: false,
 				}}
 				loop={true}
 				className="flex w-full h-full justify-center"
 				navigation={true}
-				pagination={true}
+				pagination={false}
 				keyboard={true}
 				cssMode={true}
 				modules={[Autoplay, Navigation, Pagination, Mousewheel, Keyboard]}
 			>
 				{data.map((data, index) => (
 					<SwiperSlide key={index}>
-						<Image
-							src={data.src}
-							alt="cardetail"
-							width={0}
-							height={0}
-							sizes="100vw"
-							style={{
-								width: "100%",
-								height: "auto",
-								objectFit: "contain",
-							}}
-						/>
+						<div className=" flex justify-center items-center ">
+							<Image
+								src={data.src}
+								alt="cardetail"
+								width={0}
+								height={0}
+								sizes="100vw"
+								style={{
+									width: "80%",
+									height: "auto",
+									objectFit: "contain",
+								}}
+							/>
+						</div>
+
+						<Styled.Title>{data.title}</Styled.Title>
 					</SwiperSlide>
 				))}
 			</Swiper>
-		</div>
+		</Styled.Main>
 	);
 };
 
