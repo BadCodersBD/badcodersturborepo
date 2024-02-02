b8f9194d1d4e750a8ef103b8889e9eb796699230
rakibkhanofficial <khanrakib218@gmail.com>
2024-02-02 19:17:21 +0600

    fix: fix error on backend

--- apps/hoteldb/src/middlewares/errorMiddleware.ts
+++ apps/hoteldb/src/middlewares/errorMiddleware.ts
@@ -10,7 +10,6 @@ const errorHandler = (
   err: any,
   req: Request,
   res: Response,
-  next: NextFunction
 ) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
