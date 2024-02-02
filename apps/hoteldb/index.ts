0d758cd4572de195b53e0d989f71b0e9154330b5
rakibkhanofficial <khanrakib218@gmail.com>
2024-02-02 18:02:41 +0600

    fix: fix build error

--- apps/hoteldb/index.ts
+++ apps/hoteldb/index.ts
@@ -1,18 +1,30 @@
 import dotenv from "dotenv";
 import app from "./src/app";
-// import env from "./util/validateEnv";
 import mongoose from "mongoose";
-// import express from 'express';
-const port = process.env.PORT || 5000;
-// const app = express();
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI as string,)
-    .then(() => {
-        console.log("Mongoose connected");
-        app.listen(port, () => {
-            console.log("Server running on port: " + port);
-        });
-    })
-    .catch(console.error);
+// Validate required environment variables
+const requiredEnvVariables = ["MONGO_URI", "PORT"];
 
+for (const envVar of requiredEnvVariables) {
+    if (!process.env[envVar]) {
+        console.error(`Error: Environment variable ${envVar} is not defined.`);
+        process.exit(1);
+    }
+}
+
+// Connect to MongoDB and start the server
+async function startServer() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("Mongoose connected");
+        app.listen(process.env.PORT || 5000, () => {
+            console.log("Server running on port: " + (process.env.PORT || 5000));
+        });
+    } catch (error) {
+        console.error("Mongoose connection error:", error);
+    }
+}
+
+startServer();
