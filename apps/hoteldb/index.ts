73cb7e37e63fdf2d74783284e78656d6902d08c3
Rakib Khan <khanrakib218@gmail.com>
2024-02-03 00:07:16 +0600

    Update index.ts

--- apps/hoteldb/index.ts
+++ apps/hoteldb/index.ts
@@ -15,18 +15,34 @@ for (const envVar of requiredEnvVariables) {
 }
 
 // Connect to MongoDB and start the server
-async function startServer() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+// async function startServer() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI as string);
+//     console.log("Mongoose connected");
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log("Server running on port: " + (process.env.PORT || 5000));
+//     });
+//   } catch (error) {
+//     console.error("Mongoose connection error:", error);
+//   }
+// }
+
+// startServer();
+// module.exports = app;
+
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
     console.log("Mongoose connected");
     app.listen(process.env.PORT || 5000, () => {
       console.log("Server running on port: " + (process.env.PORT || 5000));
     });
-  } catch (error) {
-    console.error("Mongoose connection error:", error);
-  }
-}
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Terminate the process on MongoDB connection failure
+  });
 
-startServer();
 module.exports = app;
 
