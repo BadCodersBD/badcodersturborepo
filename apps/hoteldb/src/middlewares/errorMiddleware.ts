06d662977e940bc8dad5ac1f046f04a3960b4d76
Rakib Khan <khanrakib218@gmail.com>
2024-02-03 18:45:32 +0600

    Update errorMiddleware.ts

--- apps/hoteldb/src/middlewares/errorMiddleware.ts
+++ apps/hoteldb/src/middlewares/errorMiddleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
-const notFound = async (req: Request, res: Response, next: NextFunction) => {
-  const error = new Error(`Not Found ${req.originalUrl}`);
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
@@ -10,10 +10,12 @@ const errorHandler = (
   err: any,
   req: Request,
   res: Response,
+  next: NextFunction
 ) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({ success: false, message: err.message });
+  next(); // Call next to pass the error to the next error handler (if any)
 };
 
 export { notFound, errorHandler };
