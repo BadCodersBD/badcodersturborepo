229cc1c71f0adb79f93cd7779223f264e2072476
rakibkhanofficial <khanrakib218@gmail.com>
2024-03-13 20:15:31 +0600

    fix: remove extra projects

--- apps/builderstate/src/_redux/reducers/counters/themeReducer/_themeRootReducer.ts
+++
@@ -1,9 +0,0 @@
-import themeToggleReducer from "./themeToggleReducer";
-
-type ReducerType<T> = (state: T, action: any) => T;
-
-export type RootReducerType<T> = Record<string, ReducerType<T>>;
-
-export const themeRootReducer: RootReducerType<any> = {
-  themeToggle: themeToggleReducer,
-} as const;
--- apps/hotelninja/src/modules/auth/_redux/reducers/_authRootreducer.ts
+++
@@ -1,5 +0,0 @@
-import authReducer from "./auth-reducer";
-
-export const authRootReducer: Record<string, typeof authReducer> = {
-  auth: authReducer,
-} as const;
